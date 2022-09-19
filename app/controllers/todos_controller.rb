828f7fc30f2beb84cbcc736cf99af75970058ab2
tsuda <tsuda@daas.co.jp>
2022-09-19 19:04:45 +0900

    edit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,7 @@ class TodosController < ApplicationController
 
   # GET /todos or /todos.json
   def index
-    @todos = Todo.all
+    @todos = Todo.all.order(id: "DESC")
   end
 
   # GET /todos/1 or /todos/1.json
