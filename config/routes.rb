eb701d8c70a112217bb44b54e9eb9c54ee02deaf
tsuda <tsuda@daas.co.jp>
2022-09-19 10:56:09 +0900

    scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'todos#index'
+  resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
